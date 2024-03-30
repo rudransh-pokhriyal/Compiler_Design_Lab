93755895d391ccac579b4881e4230f9b0b91d3d6
Rudransh Pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-03-30 16:57:57 +0000

    Prime number check program

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 2
+#define YY_END_OF_BUFFER 3
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,19 +360,19 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[8] =
     {   0,
-        2,    2,    4,    2,    3,    1,    2,    1,    0
+        0,    0,    3,    2,    1,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    2,    2,    2,
+        2,    2,    2,    2,    2,    2,    2,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +397,29 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[3] =
     {   0,
-        1,    2,    1
+        1,    2
     } ;
 
-static const flex_int16_t yy_base[11] =
+static const flex_int16_t yy_base[9] =
     {   0,
-        0,    0,    6,    0,    7,    2,    0,    0,    7,    3
+        0,    0,    4,    5,    0,    0,    5,    1
     } ;
 
-static const flex_int16_t yy_def[11] =
+static const flex_int16_t yy_def[9] =
     {   0,
-        9,    1,    9,   10,    9,   10,   10,    6,    0,    9
+        7,    1,    7,    7,    8,    8,    0,    7
     } ;
 
-static const flex_int16_t yy_nxt[11] =
+static const flex_int16_t yy_nxt[8] =
     {   0,
-        4,    5,    6,    7,    8,    9,    3,    9,    9,    9
+        4,    5,    6,    7,    3,    7,    7
     } ;
 
-static const flex_int16_t yy_chk[11] =
+static const flex_int16_t yy_chk[8] =
     {   0,
-        1,    1,    1,   10,    6,    3,    9,    9,    9,    9
+        1,    1,    8,    3,    7,    7,    7
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,10 +436,12 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "even_odd.l"
-#line 2 "even_odd.l"
-#line 442 "lex.yy.c"
-#line 443 "lex.yy.c"
+#line 1 "prime.l"
+#line 2 "prime.l"
+#include<math.h>
+int flag;
+#line 444 "lex.yy.c"
+#line 445 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -656,9 +658,9 @@ YY_DECL
 		}
 
 	{
-#line 4 "even_odd.l"
+#line 6 "prime.l"
 
-#line 662 "lex.yy.c"
+#line 664 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -685,13 +687,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 8 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 5 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -717,26 +719,30 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 5 "even_odd.l"
+#line 7 "prime.l"
 {
         int n=atoi(yytext);
-        if(n%2==0)
-        printf("Even Number\n");
-        else if(n%3==0)
-        printf("Odd Number\n");
+        flag=0;
+        for(int i=2;i<sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        printf("Prime\n");
+        else
+        printf("Not Prime\n");
        }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 13 "even_odd.l"
-{printf("Invalid\n");}
-	YY_BREAK
-case 3:
-YY_RULE_SETUP
-#line 14 "even_odd.l"
+#line 23 "prime.l"
 ECHO;
 	YY_BREAK
-#line 740 "lex.yy.c"
+#line 746 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1033,7 +1039,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 8 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1061,11 +1067,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 8 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 7);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1741,7 +1747,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 14 "even_odd.l"
+#line 23 "prime.l"
 
 
 int yywrap()
@@ -1749,9 +1755,8 @@ int yywrap()
     return 1;
 }
 
-int main()
-{
-    yylex();
-    return 1;
-}
-
+int main() 
+{ 
+  yylex(); 
+  return 0; 
+} 
