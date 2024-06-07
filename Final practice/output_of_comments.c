f6c74a8edad098d4feea0d7e0fd301cf25d8e39d
Rudransh Pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-06-07 12:07:16 +0000

    testing and output files

---
+++ Final practice/output_of_comments.c
@@ -0,0 +1,7 @@
+#include<stdio.h> 
+int main() 
+{
+    int a,b,sum=0;
+    sum=a+b;
+    printf("sum is %d",sum);
+}
---
+++ lab file/hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h> // preproccesor
+int main() // main function
+{
+    printf("Hello world");
+    return 0;
+}
